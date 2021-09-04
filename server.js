330a70f7bbce74b69727c8b83adf4c987f655c6b
Kelly Cris <kelly.cristina1995@gmail.com>
2021-09-04 14:53:28 -0300

    tentando fazer o heroku funcionar

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const express = require("express")
 const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
+
+const db = require("./src/data/database")
 
 app.listen(PORT, () => {
     console.log(`Servidor Rodando na porta ${PORT}`);
