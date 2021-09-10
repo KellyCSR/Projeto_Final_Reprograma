77c457ca9b8b622bcaf8a37e7fb28a2c09243ee3
Kelly Cris <kelly.cristina1995@gmail.com>
2021-09-09 22:59:25 -0300

    alterando o server e excluindo o app

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-const app = require("./src/app");
+const express = require('express')
+const app = express()
+
+//TODO:
+//conectar o db
+const db = require('./src/data/database')
+db.connect()
+//usar as rotas
+app.use(express.json())
+
+const clausulas = require("./src/routes/clausulaRoutes")
+app.use("/clausulas", clausulas);
+
+const contratos = require("./src/routes/contratoRoutes")
+app.use("/contratos", contratos);
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
     console.log(`Servidor Rodando na porta ${PORT}`);
 });
+
+
+module.exports = app
