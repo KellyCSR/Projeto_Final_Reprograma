9638d14163a97cb4bf008c668b159e4ead6c6e55
Kelly Cris <kelly.cristina1995@gmail.com>
2021-09-11 01:40:25 -0300

    atualizações finais

--- src/routes/clausulaRoutes.js
+++ src/routes/clausulaRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const controller = require("../controllers/clausulaController");
+const Models = require("../models/clausula")
+
+router.get('/', async(req, res) => {
+    const models = await Models.find();
+})
 
 router.get("/oi", (req, resp)=>{
     resp.status(200).send({"mensagem":"oi to aqui ta funcionando "})
 })
+
+
+
+const controller = require("../controllers/clausulaController");
 
 router.get("/", controller.getAll )
 router.get("/:id", controller.getById )
